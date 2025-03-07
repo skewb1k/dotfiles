858b62dc2a3f2e97e404d2c1c1d67337e3b04a0a
skewb1k <101928759+skewb1k@users.noreply.github.com>
2025-03-07 09:44:58 +0300

    stow

---
+++ zsh/.config/zsh/sway.zsh
@@ -0,0 +1,14 @@
+export WLR_RENDERER=vulkan 
+export GTK_THEME=Adwaita-dark
+export WLR_NO_HARDWARE_CURSORS=1 
+export XWAYLAND_NO_GLAMOR=1
+export SDL_VIDEODRIVER=wayland
+export XDG_CURRENT_DESKTOP=sway
+export XDG_SESSION_DESKTOP=sway
+export MOZ_ENABLE_WAYLAND=1
+export GDK_BACKEND=wayland
+export QT_QPA_PLATFORM=wayland
+export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
+export QT_QPA_PLATFORMTHEME=qt5ct
+
+exec sway
