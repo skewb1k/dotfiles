05c2d846e31157afdc41718989ff072771d74b67
skewb1k <101928759+skewb1k@users.noreply.github.com>
2025-03-22 22:06:03 +0300

    zsh aliasess

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,6 +1,7 @@
 alias cd=z
 alias ..="cd .."
 alias ...="cd ../.."
+alias ....="cd ../../.."
 alias ~="cd ~"
 
 alias python="python3"
@@ -11,29 +12,46 @@ alias m="make"
 
 alias sourcez="source $ZDOTDIR/.zshrc"
 
-# alias genenvexample="sed 's/=.*/=/' .env > .env.example"
-
-# dirs aliases
-alias -g winhome=/mnt/c/Users/skewbik
-
 # bun completions
 [ -s "$HOME/.bun/_bun" ] && source "$HOME/.bun/_bun"
 
 # git
 alias g='git'
 alias gp='git push'
-# alias gr='git branch -r'
-# alias gplo='git pull origin'
 alias gb="git branch"
 alias gd="git diff"
-alias gco='git checkout'
-alias gac="git add -A && git commit -v"
+alias gc="git add -A && git commit -v"
 alias gl="git log --oneline --graph --decorate --all"
 alias gs="git status"
+alias gwip='git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit -m "[WIP]: $(date)"'
+
+gsq() {
+  if [[ $# -eq 0 ]]; then
+    echo "Usage: gsq <number_of_commits_to_squash>"
+    return 1
+  fi
+
+  local num_commits="$1"
+
+  if ! [[ "$num_commits" =~ ^[0-9]+$ ]]; then
+    echo "Error: Number of commits must be a positive integer."
+    return 1
+  fi
+
+  local start_point
+  start_point=$(git log -n "$num_commits" --pretty=format:"%H" | tail -n 1)
+
+  git reset --soft HEAD~"$num_commits"
+
+  git commit -m "Squashed last $num_commits commits"
+}
+
+
+alias cdr='cd $(git rev-parse --show-toplevel)'
 
 # docker
 alias dsp="docker system prune -af && docker volume prune -af"
 
 alias sp="sudo pacman"
 
-alias sizes="du -ahd 1 | sort -hr"
+alias lsz="du -ahd 1 | sort -hr"
