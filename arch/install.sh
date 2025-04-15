b616438c413531f38b34d5c1da29771000238ebe
skewb1k <101928759+skewb1k@users.noreply.github.com>
2025-04-15 14:23:59 +0300

    [WIP]: Tue Apr 15 02:23:59 PM MSK 2025

---
+++ arch/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# File with package list
+PACKAGE_FILE="arch-pkglist.txt"
+
+if [[ ! -f "$PACKAGE_FILE" ]]; then
+  echo "Error: $PACKAGE_FILE not found!"
+  exit 1
+fi
+
+echo "Installing packages from $PACKAGE_FILE using yay..."
+yay -S --needed --noconfirm $(<"$PACKAGE_FILE")
+
