9bdb02a88b49722e6b003f847cbac687b9a1ed34
skewb1k <101928759+skewb1k@users.noreply.github.com>
2025-04-22 01:32:26 +0300

    [WIP]: Tue Apr 22 01:32:26 AM MSK 2025

--- xdg_home/nvim/.config/nvim/init.lua
+++ xdg_home/nvim/.config/nvim/init.lua
@@ -913,7 +913,16 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+    end,
+  },
+  {
+    'vague2k/vague.nvim',
+    config = function()
+      -- NOTE: you do not need to call setup if you don't want to.
+      require('vague').setup {
+        -- optional configuration here
+      }
+      vim.cmd.colorscheme 'vague'
     end,
   },
   {
