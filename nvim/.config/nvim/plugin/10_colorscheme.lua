e181bc76b0737d6e4b8c0279d64b0b6616ca9d5c
skewb1k <skewb1kunix@gmail.com>
2025-08-13 23:28:53 +0300

    nvim: disable statusline bg

--- nvim/.config/nvim/plugin/10_colorscheme.lua
+++ nvim/.config/nvim/plugin/10_colorscheme.lua
@@ -1,8 +1,8 @@
 vim.cmd("packadd vague.nvim")
 require("vague").setup({
 	italic = false,
-	-- on_highlights = function(hl)
-	-- 	hl.StatusLine.bg = nil
-	-- end,
+	on_highlights = function(hl)
+		hl.StatusLine.bg = nil
+	end,
 })
 vim.cmd("colorscheme vague")
