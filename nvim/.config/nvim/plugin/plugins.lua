7f5dc588825985f1ffe87d6cdefcc0685fddcc79
skewb1k <skewb1kunix@gmail.com>
2025-09-21 03:06:03 +0300

    nvim: fix gitsigns.nvim staged signs

--- nvim/.config/nvim/plugin/plugins.lua
+++ nvim/.config/nvim/plugin/plugins.lua
@@ -17,6 +17,14 @@ require("gitsigns").setup({
 		changedelete = { text = "~" },
 		untracked = { text = "┆" },
 	},
+	signs_staged = {
+		add = { text = "+" },
+		change = { text = "~" },
+		delete = { text = "_" },
+		topdelete = { text = "‾" },
+		changedelete = { text = "~" },
+		untracked = { text = "┆" },
+	},
 })
 
 require("mini.trailspace").setup()
