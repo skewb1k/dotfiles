f4ed8eaedcba77db513c3adc5deee1a579dd790f
skewb1k <skewb1kunix@gmail.com>
2025-09-23 02:10:18 +0300

    nvim: update mini.nvim URL to nvim-mini/

--- nvim/.config/nvim/plugin/plugins.lua
+++ nvim/.config/nvim/plugin/plugins.lua
@@ -2,7 +2,7 @@ vim.pack.add({
 	{ src = "https://github.com/brenoprata10/nvim-highlight-colors" },
 	{ src = "https://github.com/lewis6991/gitsigns.nvim" },
 	{ src = "https://github.com/ibhagwan/fzf-lua" },
-	{ src = "https://github.com/echasnovski/mini.trailspace" },
+	{ src = "https://github.com/nvim-mini/mini.trailspace" },
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
 	{ src = "https://github.com/skewb1k/colorscheme-preview.nvim" },
 }, { load = true })
