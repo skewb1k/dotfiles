e9a73269ed69f9250082b8bf7e36f0c34bfff27c
skewb1k <skewb1kunix@gmail.com>
2025-09-26 23:50:40 +0300

    WIP

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -49,3 +49,18 @@ vim.o.wildmode = "noselect:lastused"
 
 vim.o.path = "**"
 vim.o.grepprg = "rg --vimgrep --hidden" -- respect .gitignore
+
+-- neovide configs
+if vim.g.neovide then
+	vim.o.guifont = "JetBrainsMono Nerd Font"
+	vim.g.neovide_hide_mouse_when_typing = true
+	vim.g.neovide_position_animation_length = 0
+	vim.g.neovide_cursor_animation_length = 0
+	vim.g.neovide_cursor_trail_size = 0
+	vim.g.neovide_cursor_short_animation_length = 0
+	vim.g.neovide_cursor_animate_in_insert_mode = false
+	vim.g.neovide_cursor_animate_command_line = false
+	vim.g.neovide_cursor_smooth_blink = false
+	vim.g.neovide_scroll_animation_far_lines = 0
+	vim.g.neovide_scroll_animation_length = 0
+end
