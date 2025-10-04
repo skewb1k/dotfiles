e5ae390398065256a34f467c4154b0544ac65199
skewb1k <skewb1kunix@gmail.com>
2025-10-04 20:28:53 +0300

    nvim: temporarily disable 'colorscheme-preview.nvim'

--- nvim/.config/nvim/plugin/plugins.lua
+++ nvim/.config/nvim/plugin/plugins.lua
@@ -3,7 +3,7 @@ vim.pack.add({
 	{ src = "https://github.com/lewis6991/gitsigns.nvim" },
 	{ src = "https://github.com/nvim-mini/mini.trailspace" },
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
-	{ src = "https://github.com/skewb1k/colorscheme-preview.nvim" },
+	-- { src = "https://github.com/skewb1k/colorscheme-preview.nvim" },
 }, { load = true, confirm = false })
 
 require("nvim-highlight-colors").setup({})
