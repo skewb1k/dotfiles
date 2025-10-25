cea1196806075a5fc0baf492844cc53db920dc23
skewb1k <skewb1kunix@gmail.com>
2025-10-25 13:45:41 +0300

    nvim: set augroups for all autocmds

--- nvim/.config/nvim/plugin/autocmds.lua
+++ nvim/.config/nvim/plugin/autocmds.lua
@@ -1,11 +1,10 @@
 vim.api.nvim_create_autocmd("TextYankPost", {
-	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
-	callback = function()
-		vim.hl.on_yank()
-	end,
+	group = vim.api.nvim_create_augroup("highlight-yank", {}),
+	callback = vim.hl.on_yank,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
+	group = vim.api.nvim_create_augroup("ft-tabstop", {}),
 	pattern = { "json", "jsonc", "lua" },
 	callback = function()
 		vim.bo.tabstop = 2
@@ -14,19 +13,17 @@ vim.api.nvim_create_autocmd("FileType", {
 
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = "*",
-	desc = "Disable auto-comment on new line",
+	group = vim.api.nvim_create_augroup("disable-autocomment", {}),
 	callback = function()
 		vim.opt.formatoptions:remove({ "c", "r", "o" })
 	end,
 })
 
 vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("lsp-attach", {}),
 	callback = function(ev)
 		local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
 		if client:supports_method("textDocument/completion") then
-			-- Optional: trigger autocompletion on EVERY keypress. May be slow!
-			-- local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
-			-- client.server_capabilities.completionProvider.triggerCharacters = chars
 			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
 		end
 		vim.lsp.semantic_tokens.enable(false)
