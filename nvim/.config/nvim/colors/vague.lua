ec23339aaad0072d573c75c2ecbb50f1b638acf3
skewb1k <skewb1kunix@gmail.com>
2025-10-25 13:10:13 +0300

    nvim: pull vague.nvim with hl-Whitespace fix

--- nvim/.config/nvim/colors/vague.lua
+++ nvim/.config/nvim/colors/vague.lua
@@ -1,4 +1,3 @@
 vim.cmd.runtime("PACK colors/vague.lua")
 vim.api.nvim_set_hl(0, "StatusLine", { link = "Normal" })
 vim.api.nvim_set_hl(0, "Trailspace", { bg = vim.api.nvim_get_hl(0, { name = "Error" }).fg })
-vim.api.nvim_set_hl(0, "Whitespace", { fg = "#252530" })
