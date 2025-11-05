cadb886abf6af92d1ed5a1c9a4a7cf7e80ab17b8
skewb1k <skewb1kunix@gmail.com>
2025-11-05 06:20:30 +0300

    nvim: update `@punctuation.delimiter.test` color

--- nvim/.config/nvim/colors/vague.lua
+++ nvim/.config/nvim/colors/vague.lua
@@ -1,4 +1,4 @@
 vim.cmd.runtime("PACK colors/vague.lua")
 vim.api.nvim_set_hl(0, "StatusLine", { link = "Normal" })
 vim.api.nvim_set_hl(0, "Trailspace", { bg = vim.api.nvim_get_hl(0, { name = "Error" }).fg })
-vim.api.nvim_set_hl(0, "@punctuation.delimiter.test", { link = "Identifier" })
+vim.api.nvim_set_hl(0, "@punctuation.delimiter.test", { link = "Keyword" })
