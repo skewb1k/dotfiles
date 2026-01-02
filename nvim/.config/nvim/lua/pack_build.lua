b3fa8fa7df1a94ea1cdac78a533e2501f8e8e06f
skewb1k <skewb1kunix@gmail.com>
2026-01-02 09:28:24 +0300

    nvim: 'pack_build' run synchronously

--- nvim/.config/nvim/lua/pack_build.lua
+++ nvim/.config/nvim/lua/pack_build.lua
@@ -3,14 +3,13 @@ vim.api.nvim_create_autocmd("PackChanged", {
 	group = vim.api.nvim_create_augroup("vim.pack-build-system", {}),
 	desc = "Execute the plugin's `data.build`",
 	callback = function(e)
-		vim.print(e)
 		if e.data.kind == "delete" then
 			return
 		end
 
 		local build = (e.data.spec.data or {}).build
 		if type(build) == "string" then
-			vim.system({ "sh" }, { stdin = "cd " .. e.data.path .. ";" .. build })
+			vim.system({ "sh" }, { stdin = "cd " .. e.data.path .. ";" .. build }):wait()
 		elseif type(build) == "function" then
 			pcall(build, e.data)
 		end
