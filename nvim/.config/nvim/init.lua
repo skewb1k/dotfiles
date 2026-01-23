8d9124583278fdc1d97c5b818ab5733c1f489448
skewb1k <skewb1kunix@gmail.com>
2026-01-23 16:42:32 +0300

    nvim: disable `packbuild`

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -32,7 +32,6 @@ vim.o.inccommand = "split"
 vim.o.list = true
 vim.opt.listchars = { tab = "> ", space = "·" }
 
-require("packbuild")
 vim.pack.add({
 	{ src = "https://github.com/skewb1k/vague-quiet.nvim" },
 }, { confirm = false })
